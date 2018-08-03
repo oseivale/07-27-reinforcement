4e7c3943f96f50a979f06e9c2c07217308600dc4
Valerie Osei <valerie.osei@gmail.com>
2018-08-03 00:20:23 -0400

    created new class, played around with methods

---
+++ exercise01.rb
@@ -0,0 +1,25 @@
+
+class Task
+
+attr :description, :due_date
+
+  def initialize(description, due_date)
+    @description = description
+    @due_date = due_date
+  end
+
+end
+
+
+sweeping = Task.new("sweeping the floor", "July 5th")
+wash_dishes = Task.new("Clean plates", "July 7th")
+laundry = Task.new("Wash clothes", "July 11th")
+
+puts sweeping.description
+puts sweeping.due_date
+
+puts wash_dishes.description
+puts wash_dishes.due_date
+
+puts laundry.description
+puts laundry.due_date
